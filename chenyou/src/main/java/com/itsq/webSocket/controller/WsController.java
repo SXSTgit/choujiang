2fbf2fe8cd128e1f3acc411929eebfcdb510d559
sxs <1085432162@qq.com@qq.com>
2020-06-09 01:10:16 +0800

    第九次提交

--- chenyou/src/main/java/com/itsq/webSocket/controller/WsController.java
+++ chenyou/src/main/java/com/itsq/webSocket/controller/WsController.java
@@ -1,12 +1,15 @@
 package com.itsq.webSocket.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.itsq.common.base.BaseController;
 import com.itsq.common.bean.Response;
 import com.itsq.common.redis.RedisUtils;
 import com.itsq.pojo.dto.PageParametersDto;
 import com.itsq.pojo.dto.PlayersDto;
+import com.itsq.pojo.entity.PlayerBoxArms;
 import com.itsq.service.resources.PlayerBoxArmsService;
 import com.itsq.service.resources.PlayersService;
+import com.itsq.utils.PagesUtil;
 import com.itsq.utils.RandomUtil;
 import com.itsq.webSocket.model.RequestMessage;
 import com.itsq.webSocket.model.ResponseMessage;
@@ -97,22 +100,24 @@ public class WsController extends BaseController {
         pageParametersDto.setPageNum(1);
         pageParametersDto.setPageSize(20);
         pageParametersDto.setOrderByField("1");
-        playerBoxArmsService.selectPlayerBoxArmsPage(pageParametersDto);
+        PagesUtil<PlayerBoxArms> playerBoxArmsPagesUtil = playerBoxArmsService.selectPlayerBoxArmsPage(pageParametersDto);
         pageParametersDto.setPageNum(1);
+
         pageParametersDto.setPageSize(1);
         pageParametersDto.setOrderByField("2");
-
+        PagesUtil<PlayerBoxArms> playerBoxArmsPagesUtil1 = playerBoxArmsService.selectPlayerBoxArmsPage(pageParametersDto);
         int playerCount = playersService.selectPlayerCount();
         int boxCount =   playerBoxArmsService.selectUpCount(0);
         int uPCount =    playerBoxArmsService.selectUpCount(1);
-        Map<String ,Object > map=new HashMap<>();
-        map.put("login", redisUtil.get("count"));
-        map.put("playerCount",playerCount);
-        map.put("boxCount",boxCount);
-        map.put("uPCount",uPCount);
-        map.put("lunbo", playerBoxArmsService.selectPlayerBoxArmsPage(pageParametersDto).toString());
-        map.put("zuijia",  playerBoxArmsService.selectPlayerBoxArmsPage(pageParametersDto).toString());
+
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("login", redisUtil.get("count"));
+        jsonObject.put("playerCount",playerCount);
+        jsonObject.put("boxCount",boxCount);
+        jsonObject.put("uPCount",uPCount);
+        jsonObject.put("lunbo", playerBoxArmsPagesUtil);
+        jsonObject.put("zuijia",  playerBoxArmsPagesUtil1);
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        messagingTemplate.convertAndSend("/topic/callback", "定时推送消息时间: " + map.toString());
+        messagingTemplate.convertAndSend("/topic/callback",  jsonObject.toString());
     }
 }
