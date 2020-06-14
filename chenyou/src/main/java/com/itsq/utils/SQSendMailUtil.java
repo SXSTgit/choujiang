341fea88d930f68880bd8076bdbb2f33f115efdf
qingang <734654628@qq.com>
2020-06-15 01:20:12 +0800

    yunrise增加

--- chenyou/src/main/java/com/itsq/utils/SQSendMailUtil.java
+++ chenyou/src/main/java/com/itsq/utils/SQSendMailUtil.java
@@ -29,13 +29,14 @@ public class SQSendMailUtil {
         // 发件人的 邮箱 和 密码（替换为自己的邮箱和密码）
         // PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）,
         // 对于开启了独立密码的邮箱, 这里的邮箱密码必需使用这个独立密码（授权码）。
-        String myEmailAccount = "1085432162@qq.com";
-        String myEmailPassword = "";
+        String myEmailAccount = "register@boxgo.cc";
+        String myEmailPassword = "duobao@2020";
         // 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
         // 网易163邮箱的 SMTP 服务器地址为: smtp.163.com
-        String myEmailSMTPHost = "smtp.qq.com";
+//        String myEmailSMTPHost = "smtpout.asia.secureserver.net";
+        String myEmailSMTPHost = "smtpout.asia.secureserver.net";
         // 收件人邮箱（替换为自己知道的有效邮箱）
-        String[] toMailAccountList = new String[]{"1849207990@qq.com"};
+        String[] toMailAccountList = new String[]{"734654628@qq.com"};
         SQSendMailUtil.sendMail(myEmailAccount, myEmailPassword, "", toMailAccountList, "", myEmailSMTPHost, "测试发送邮件", "测试发送邮件");
 
     }
